2286b282f5af5cb5467c8c39a24c00c8961477bc
brob1115 <broberts111592@gmail.com>
2023-08-23 03:53:19 -0500

    yolo

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import Nav from "./Nav";
+import Carousel from "./Carousel";
+import Bouncy from "./Bouncy";
+import Games from "./Games";
+import Equipment from "./Equipment";
+import Contact from "./Contact";
+
+function App() {
+	return (
+		<>
+			<Nav />
+			<Carousel />
+			<Bouncy />
+			<Games />
+			<Equipment />
+			<Contact />
+		</>
+	);
+}
+
+export default App;
